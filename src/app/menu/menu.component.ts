95571265ad09708fa9d8bbd3e37b33e102f44e99
simonecipriani <ciprianisimone95@gmail.com>
2023-05-09 11:59:47 +0200

    Ver 6.3

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
   // --- componente che gestisce la rappresentazione del menu di navigazione su ogni pagina ---
 
@@ -15,6 +16,29 @@ export class MenuComponent {
   // --- metodo per controllare se l'utente è attualmente loggato, controllando la variabile globale isLogged ---
   isLoggedIn():boolean {
     return this.auth.getLoggedIn();
+  }
+
+  // --- controllo subito se siamo su disp. mobili ---
+  ngOnInit(): void {
+    this.checkSmart();
+  }
+
+  // --- funzione che controlla se ci troviamo su disp.mobili, inserita
+  // per fare in modo che il pulsante "mia cineteca" vada dentro il menu
+  // hamburger solo su disp mobili (tramite evento window:resize in HTML) ---
+  checkSmart():void {
+    console.log("heiu");
+    if(window.innerWidth <= 992) {
+      environment.isSmart = true;
+    }
+    else {
+      environment.isSmart = false;
+    }
+  }
+
+  // --- controllo il valore del booleano di ambiente per vedere se siamo su disp.mobili ---
+  isSmart():boolean {
+    return environment.isSmart;
   }
   
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
     USER_API_BASE_URL: "http://localhost:3000/",
+    isSmart: false
 };
